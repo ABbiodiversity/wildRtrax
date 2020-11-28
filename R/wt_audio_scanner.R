c8fca117043b07563044db49928ab1e5fb1fe696
Alex MacPhail <agmacpha@ualberta.ca>
2020-11-28 12:06:07 -0700

    modified gps enabled argument. updated vignette.

--- R/wt_audio_scanner.R
+++ R/wt_audio_scanner.R
@@ -60,8 +60,7 @@ wt_audio_scanner <- function(path, file_type) {
       julian = lubridate::yday(recording_date_time),
       year = lubridate::year(recording_date_time),
       gps_enabled = dplyr::case_when(
-        grepl('$', file_name) ~ TRUE,
-        TRUE ~ FALSE)
+        grepl('$', file_name) ~ TRUE),
       year = lubridate::year(recording_date_time)
     ) %>%
     dplyr::arrange(location, recording_date_time) %>%
