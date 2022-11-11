fb40d295daca5a6714bc3e1084c768175ce30ef4
agmacpha <agmacpha@ualberta.ca>
2022-11-11 12:57:40 -0700

    Populate linking media family of functions with wt_make_tasks and wt_kaleido_tags

---
+++ R/linking-media-to-wildtrax.R
@@ -0,0 +1,175 @@
+#' Pre-process acoustic data for use in WildTrax
+#'
+#' @description
+#'
+#' `wt_make_tasks` uses a `wt_audio_scanner` tibble to create a task template to upload to a WildTrax project. `wt_kaleido_tags` takes the output from Wildlife Acoustics'
+#' Kaleidoscope software () in order to upload the hits as tags to a project for verification and publication.
+#'
+#' Learn more in `vignette("linking-media-to-wildtrax")`.
+#'
+#' Scan and extract metadata from audio data
+#'
+#' @description
+#'
+#' Using a `wt_audio_scanner` tibble, generate a list of tasks for upload to WildTrax
+#'
+#' @section `wt_make_tasks` details:
+#'
+#' @param input Character; An input `wt_audio_scanner` tibble. If not a `wt_audio_scanner` tibble, the data must contain at minimum the location, recording_date_time and file_path as column headers.
+#' @param output Character; Path where the output task csv file will be stored
+#' @param task_method Character; Method type of the task. Options are 1SPM, 1SPT and None. See Methods in WildTrax for more details.
+#' @param task_length Numeric; Task length in seconds. Must be an between 1 - 1800 and can be up to two decimal places.
+#'
+#' @import dplyr tidyr readr pipeR stringr lubridate tibble
+#' @importFrom lubridate ymd_hms with_tz
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' wt_make_tasks(input = my_audio_tibble, output = tasks.csv)
+#' }
+#'
+#' @return A csv formatted as a WildTrax task template
+#'
+#'
+
+wt_make_tasks <- function(input, output, task_method = c("1SPM","1SPT","None"), task_length) {
+
+  if (rlang::env_has(rlang::current_env(), input)) {
+    task_prep <- input
+  } else {
+    stop("Couldn't find this input in the current environment.")
+  }
+
+  req_cols <- c("file_path","location","recording_date_time")
+
+  if(req_cols %in% colnames(task_prep)) {
+    next
+  } else {
+    stop("One of the required columns are absent from this data input. Ensure you have file_path, location, recording_date_time, length")
+  }
+
+  req_methods <- c("1SPM","1SPT","None")
+
+  if(task_method %in% req_methods){
+    next
+  } else {
+    stop("This isn't an accepted method. Please use 1SPM, 1SPT or None.")
+  }
+
+  if(is.numeric(task_length) & task_length >= 1 & task_length < 1800){
+    next
+  } else {
+    stop("task_length must be a number between 1 and 1800.")
+  }
+
+  tasks <- task_prep %>%
+    dplyr::select(location, recording_date_time, length_seconds) %>%
+    dplyr::distinct() %>%
+    dplyr::rename("taskLength" = 3) %>%
+    dplyr::mutate(taskLength = task_length) %>%
+    #Add the necessary task columns
+    tibble::add_column(method = task_method, .after = "recording_date_time") %>%
+    tibble::add_column(status = "New", .after = "taskLength") %>%
+    tibble::add_column(transcriber = "", .after = "status") %>%
+    tibble::add_column(rain = "", .after = "transcriber") %>%
+    tibble::add_column(wind = "", .after = "rain") %>%
+    tibble::add_column(industryNoise = "", .after = "wind") %>%
+    tibble::add_column(otherNoise = "", .after = "industryNoise") %>%
+    tibble::add_column(audioQuality = "", .after = "otherNoise") %>%
+    tibble::add_column(taskComments = "", .after = "audioQuality") %>%
+    tibble::add_column(internal_task_id = "", .after = "taskComments")
+  #{if (im_feeling_lucky = T) sample_frac(runif(1,0,1), replace = F)}
+
+  return(write.csv(tasks, path = output, row.names = F))
+
+  print("Converted list of recordings to WildTrax tasks. Go to your WildTrax organization > Recordings Tab > Manage > Upload Recordings.
+        Then go to your WildTrax project > Manage > Upload Tasks to upload the csv of tasks.")
+
+}
+
+#' Takes the classifier output from Wildlife Acoustics Kaleidoscope and converts them into a WildTrax tag template for upload
+#'
+#' @param input Character; The path to the input csv
+#' @param output Character; Path where the output file will be stored
+#' @param tz Character; Assigns a timezone to the recording files. Use `OlsonNames()` to get a list of valid names.
+#' @param freq_bump Boolean; Set to TRUE to add a buffer to the frequency values exported from Kaleidoscope. Helpful for getting more context around a signal in species verification
+#'
+#' @import dplyr tidyr readr pipeR stringr lubridate tibble
+#' @importFrom lubridate ymd_hms with_tz
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' wt_kaleido_tags(input = input.csv, output = tags.csv, tz = "", freq_bump = T)
+#' }
+#'
+#' @return A csv formatted as a WildTrax tag template
+
+wt_kaleido_tags <- function (input, output, freq_bump = T) {
+
+  #Check to see if the input exists and reading it in
+  if (file.exists(input)) {
+    in_tbl <- readr::read_csv(input, col_names = TRUE, na = c("", "NA"), col_types = cols())
+  } else {
+    stop ("File cannot be found")
+  }
+
+  #Cleaning things up for the tag template
+  in_tbl_wtd <- in_tbl %>%
+    dplyr::select(INDIR, `IN FILE`, DURATION, OFFSET, Dur, DATE, TIME, `AUTO ID*`, Fmin, Fmax) %>%
+    tidyr::separate(`IN FILE`, into = c("location", "recording_date_time"), sep = "(?:_0\\+1_|_|__0__|__1__)", extra = "merge", remove = F) %>%
+    dplyr::select(-(DATE:TIME)) %>%
+    dplyr::relocate(location) %>%
+    dplyr::relocate(recording_date_time, .after = location) %>%
+    dplyr::mutate(recording_date_time = stringr::str_remove(recording_date_time,'.+?(?:__)')) %>%
+    # Create date/time fields
+    dplyr::mutate(recording_date_time = lubridate::with_tz(lubridate::ymd_hms(recording_date_time), tzone = tz, roll = TRUE)) %>% #Apply a time zone if necessary
+    dplyr::rename("taskLength" = 5,
+                  "startTime" = 6,
+                  "tagLength" = 7,
+                  "species" = 8,
+                  "minFreq" = 9,
+                  "maxFreq" = 10) %>%
+    dplyr::select(-(INDIR:`IN FILE`)) %>%
+    # Updating names to WildTrax species codes
+    dplyr::mutate(species = case_when(species == "NoID" ~ "UBAT",
+                                      species == "H_freq_Bat" ~ "HighF",
+                                      species == "L_freq_Bat" ~ "LowF",
+                                      TRUE ~ species),
+                  startTime = dplyr::case_when(startTime == 0 ~ 0.1, TRUE ~ startTime)) %>% #Adjusting startTime parameter
+    tibble::add_column(method = "1SPT", .after = "recording_date_time") %>%
+    tibble::add_column(transcriber = "Not Assigned", .after = "taskLength") %>%
+    dplyr::group_by(location, recording_date_time, taskLength, species) %>%
+    dplyr::mutate(speciesIndividualNumber = row_number()) %>%
+    dplyr::ungroup() %>%
+    tibble::add_column(vocalization = "", .after = "speciesIndividualNumber") %>%
+    tibble::add_column(abundance = 1, .after= "vocalization") %>%
+    dplyr::mutate(vocalization = case_when(species == "Noise" ~ "Non-vocal", TRUE ~ "Call")) %>%
+    tibble::add_column(internal_tag_id = "", .after = "maxFreq") %>%
+    dplyr::mutate(recording_date_time = as.character(recording_date_time)) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(tagLength = dplyr::case_when(tagLength > taskLength ~ taskLength, TRUE ~ tagLength)) %>%
+    dplyr::mutate(tagLength = dplyr::case_when(is.na(tagLength) ~ taskLength - startTime, TRUE ~ tagLength),
+                  minFreq = dplyr::case_when(is.na(minFreq) ~ 12000, TRUE ~ minFreq * 1000),
+                  maxFreq = dplyr::case_when(is.na(maxFreq) ~ 96000, TRUE ~ maxFreq * 1000)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate_at(vars(taskLength,minFreq,maxFreq), ~round(.,2)) %>%
+    #Apply the frequency bump (+/- 10000 Hz)
+    dplyr::mutate(minFreq = dplyr::case_when(freq_bump == TRUE ~ minFreq - 10000, TRUE ~ minFreq),
+                  maxFreq = dplyr::case_when(freq_bump == TRUE ~ maxFreq + 10000, TRUE ~ maxFreq)) %>%
+    dplyr::relocate(taskLength, .after = method) %>%
+    dplyr::relocate(startTime, .after = abundance) %>%
+    dplyr::relocate(tagLength, .after = startTime) %>%
+    dplyr::relocate(minFreq, .after = tagLength) %>%
+    dplyr::relocate(maxFreq, .after = minFreq) %>%
+    dplyr::relocate(internal_tag_id, .after = maxFreq)
+
+  #Write the file
+  return(write.csv(in_tbl_wtd, file = output, row.names = F))
+
+  print("Converted to WildTrax tags. Go to your WildTrax project > Manage > Upload Tags.")
+
+}
+
+
