8b52789c28f697fcd42258e7865d414ea7007dba
ecknight <ellyknight@gmail.com>
2022-12-01 10:53:30 -0700

    Initial commit for replace_tmtt

---
+++ R/wt_replace_tmtt.R
@@ -0,0 +1,50 @@
+#' Replace 'TMTT' abundance entries with model-predicted values.
+#'
+#' @description This function uses a lookup table of model-predicted values to replace 'TMTT' entries in listener-processed ARU data from WildTrax. The model-predicted values were produced using estimated abundances for 'TMTT' entries in mixed effects model with a Poisson distribution and random effects for species and observer.
+#'
+#' @param data Dataframe of WildTrax observations, for example the summary report.
+#' @param calc Method to convert model predictions to integer. See ?round for details.
+#' @import dplyr
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' dat.tmtt <- replace_tmtt(dat, calc="round")
+#' }
+#' @return A dataframe identical to input with 'TMTT' entries in the abundance column replaced by integer values.
+
+
+replace_tmtt <- function(data, calc="round"){
+
+  #load tmtt lookup table
+  .tmtt <- read.csv(system.file("data/tmtt_predictions.csv", package="wildRtrax"))
+
+  #wrangle to tmtts only
+  dat.tmtt <- data %>%
+    dplyr::filter(abundance=="TMTT")
+
+  #replace values with random selection from bootstraps
+  dat.abun <- dat.tmtt %>%
+    mutate(species_code = ifelse(species_code %in% .tmtt$species_code, species_code, "species"),
+           user_id = as.integer(ifelse(user_id %in% .tmtt$user_id, user_id, 0))) %>%
+    data.frame() %>%
+    left_join(.tmtt, by=c("species_code", "user_id", "boot"))
+
+  #summarize predicted values
+  dat.abun$abundance <- as.integer(case_when(calc=="round" ~ round(pred),
+                                   calc=="ceiling" ~ ceiling(pred),
+                                   calc=="floor" ~ floor(pred)))
+
+  #join back to data
+  out <- dat.abun  %>%
+    dplyr::select(colnames(dat)) %>%
+    rbind(data %>%
+            dplyr::filter(abundance!="TMTT"))
+
+  #return the unmarked object
+  return(out)
+
+  #remove the lookup table
+  rm(.tmtt)
+
+}
