5dc1d01945a3628d3fd2269e9e2325c8322a331b
agmacpha <agmacpha@ualberta.ca>
2023-03-14 21:11:06 -0600

    Fresh render. Continued long-form content additions.

---
+++ R/reporting.R
@@ -0,0 +1,22 @@
+#' Ttitle
+#' Description of family
+#'
+#' @section `wt_report` details:
+#'
+#' @description
+#'
+#' @param data
+#' @param type basic, advanced
+#' @param style abmi.themes, gitbook, flaired
+#'
+#' @import markdown
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' wt_report(data = my_data, type = c("basic", "advanced"), style = "gitbook")
+#' }
+#'
+#' @return A pdf document with the report
+
+wt_report()
