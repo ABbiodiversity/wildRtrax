7f3d8af0dbd1195fa9a0604fa438ab285d5bfe1d
agmacpha <agmacpha@ualberta.ca>
2023-03-15 16:59:01 -0600

    Edit to vignettes.

--- R/reporting.R
+++ R/reporting.R
@@ -19,4 +19,29 @@
 #'
 #' @return A pdf document with the report
 
-wt_report()
+wt_report <- function (data, output_file = "report.html", output_dir = getwd(), open) {
+
+  in_tbl_wtd <- raw_basic_data
+
+  aplot <- in_tbl_wtd %>%
+    group_by(species_code) %>%
+    tally() %>%
+    filter(n > 10) %>%
+    ggplot(., aes(x=reorder(species_code,n),y=n,fill=species_code)) +
+    geom_bar(stat="identity") +
+    coord_flip() +
+    abmi.themes::scale_fill_abmi() +
+    abmi.themes::theme_abmi()
+
+  save(aplot,"aplot.R")
+
+  stitch(aplot, system.file("misc", "knitr-template.Rmd", package = "knitr"))
+
+  report_path <- path.expand(file.path(output_dir, output_file))
+
+  if (open == "auto") {
+    browseURL(report_path)
+  }
+
+}
+
