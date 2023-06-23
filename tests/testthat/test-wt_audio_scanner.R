bb4e99f41a8ec68d0a6cc2a4e2948cac8d9a17e0
agmacpha <agmacpha@ualberta.ca>
2023-06-23 11:15:31 -0600

    rebuilding vignettes

--- tests/testthat/test-wt_audio_scanner.R
+++ tests/testthat/test-wt_audio_scanner.R
@@ -1,5 +1,5 @@
 testthat::test_that("Test wt_audio_scanner", {
-  f <- wt_audio_scanner("/users/alexandremacphail/wildRtrax/tests/testthat/", file_type = "wav", extra_cols = TRUE)
+  f <- wt_audio_scanner(".", file_type = "wav", extra_cols = TRUE)
   rows <- nrow(f)
   result <- (rows == 1)
   testthat::expect_no_error(result)
