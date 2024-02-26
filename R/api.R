df5bdd3eccfaa4913ac047a804d128bb52330bb9
agmacpha <agmacpha@ualberta.ca>
2024-02-26 13:21:45 -0700

    checks

--- R/api.R
+++ R/api.R
@@ -258,7 +258,8 @@ wt_download_report <- function(project_id, sensor_id, reports, weather_cols = TR
     purrr::map(~file.rename(.x, gsub("[:()?!~;]", "", .x)))
   files.full <- list.files(td, pattern= "*.csv", full.names = TRUE)
   files.less <- basename(files.full)
-  x <- purrr::map(.x = files.full, .f = ~ suppressWarnings(readr::read_csv(., show_col_types = F, skip_empty_rows = T, col_types = list(abundance = col_character())))) %>%
+  x <- purrr::map(.x = files.full, .f = ~ suppressWarnings(readr::read_csv(., show_col_types = F,
+                                                                           skip_empty_rows = T, col_types = list(abundance = col_character())))) %>%
     purrr::set_names(files.less)
 
 
