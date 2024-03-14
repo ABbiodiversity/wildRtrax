808ea1815467c4de346da122454cc8d434545f45
agmacpha <agmacpha@ualberta.ca>
2024-03-14 14:19:09 -0600

    even better tests

--- tests/testthat/test-cam.R
+++ tests/testthat/test-cam.R
@@ -10,5 +10,16 @@ test_that("Downloading CAM report", {
   expect_true(!is.null(abmi_amph_cam))
 })
 
-##wt_summarise_cam
-##wt_ind_detect
+test_that("Individual detections", {
+  abmi_amph_cam <- wt_download_report(391, 'CAM', 'main', FALSE)
+  ind_detections <- wt_ind_detect(abmi_amph_cam, threshold, units = "minutes", datetime_col = image_date_time, remove_human = TRUE, remove_domestic = TRUE)
+  expect_true(nrow(abmi_amph_cam) > nrow(ind_detections))
+
+})
+
+test_that("Individual detections", {
+  abmi_amph_cam <- wt_download_report(391, 'CAM', 'main', FALSE)
+  ind_detections <- wt_ind_detect(abmi_amph_cam, threshold = 10, units = "minutes", datetime_col = image_date_time, remove_human = TRUE, remove_domestic = TRUE)
+  summary <- wt_summarise_cam(detect_data = ind_detections, raw_data = abmi_amph_cam, time_interval = "day", variable = "detections", output_format = "wide")
+  except_true(ncol(summary) > ncol(ind_detections))
+})
--- tests/testthat/test-pc.R
+++ tests/testthat/test-pc.R
@@ -24,46 +24,46 @@ test_that("Attempting PC as ARU report", {
 
 test_that('QPAD for PC together false', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T, sensor = 'PC')
-  pc_wide <- wt_make_wide(pc_tidy, sound = "all", sensor = 'PC')
-  pc_qpad <- wt_qpad_offsets(pc_wide, species = "all", version = 3, together = F, sensor = 'PC')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T)
+  pc_wide <- wt_make_wide(pc_tidy, sound = "all")
+  pc_qpad <- wt_qpad_offsets(pc_wide, species = "all", version = 3, together = F)
   expect_true(ncol(pc_wide) > ncol(pc_qpad))
 })
 
 test_that('QPAD for PC together true', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T, sensor = 'PC')
-  pc_wide <- wt_make_wide(pc_tidy, sound = "all", sensor = 'PC')
-  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = T, sensor = 'PC')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T)
+  pc_wide <- wt_make_wide(pc_tidy, sound = "all")
+  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = T)
   expect_true(ncol(pc_qpad) > ncol(pc_wide))
 })
 
 test_that('QPAD for PC zerofill false together false', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = F, sensor = 'PC')
-  pc_wide <- wt_make_wide(pc_tidy, sound = "all", sensor = 'PC')
-  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = F, sensor = 'PC')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = F)
+  pc_wide <- wt_make_wide(pc_tidy, sound = "all")
+  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = F)
   expect_true(ncol(pc_wide) > ncol(pc_qpad))
 })
 
 test_that('QPAD for PC zetrofill false', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = F, sensor = 'PC')
-  pc_wide <- wt_make_wide(pc_tidy, sound = "all", sensor = 'PC')
-  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = T, sensor = 'PC')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = F)
+  pc_wide <- wt_make_wide(pc_tidy, sound = "all")
+  pc_qpad <- wt_qpad_offsets(pc_wide, species = "OVEN", version = 3, together = T)
   expect_true(ncol(pc_qpad) > ncol(pc_wide))
 })
 
 test_that('Occupancy for PC', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T, sensor = 'PC')
-  occu <- wt_format_occupancy(pc_tidy, species = "OVEN")
-  expect_condition(class(occu)[1] == 'unmarkedFrameOccu')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T)
+  pc_occu <- wt_format_occupancy(pc_tidy, species = "all")
+  expect_condition(class(pc_occu)[1] == 'unmarkedFrameOccu')
 })
 
 
 test_that('Error for TMTT', {
   pc_data <- wt_download_report(887, 'PC', 'main', FALSE)
-  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T, sensor = 'PC')
+  pc_tidy <- wt_tidy_species(pc_data, remove = c("unknown"), zerofill = T)
   expect_error(wt_replace_tmtt(pc_tidy, calc = 'round'))
 })
