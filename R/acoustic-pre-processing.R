f5cc2a34fab2fb459cee643103043f0d8ea1d048
agmacpha <agmacpha@ualberta.ca>
2024-03-14 08:12:20 -0600

    remove tz from kaleidoscope_tags

--- R/acoustic-pre-processing.R
+++ R/acoustic-pre-processing.R
@@ -760,7 +760,6 @@ wt_make_aru_tasks <- function(input, output=NULL, task_method = c("1SPM","1SPT",
 #'
 #' @param input Character; The path to the input csv
 #' @param output Character; Path where the output file will be stored
-#' @param tz Character; Assigns a timezone to the recording files. Use `OlsonNames()` to get a list of valid names.
 #' @param freq_bump Boolean; Set to TRUE to add a buffer to the frequency values exported from Kaleidoscope. Helpful for getting more context around a signal in species verification
 #'
 #' @import dplyr tidyr readr tibble
@@ -768,12 +767,12 @@ wt_make_aru_tasks <- function(input, output=NULL, task_method = c("1SPM","1SPT",
 #'
 #' @examples
 #' \dontrun{
-#' wt_kaleidoscope_tags(input = input.csv, output = tags.csv, tz = "", freq_bump = T)
+#' wt_kaleidoscope_tags(input = input.csv, output = tags.csv, freq_bump = T)
 #' }
 #'
 #' @return A csv formatted as a WildTrax tag template
 
-wt_kaleidoscope_tags <- function (input, output, tz, freq_bump = T) {
+wt_kaleidoscope_tags <- function (input, output, freq_bump = T) {
 
   #Check to see if the input exists and reading it in
   if (file.exists(input)) {
@@ -785,13 +784,13 @@ wt_kaleidoscope_tags <- function (input, output, tz, freq_bump = T) {
   #Cleaning things up for the tag template
   in_tbl_wtd <- in_tbl %>%
     dplyr::select(INDIR, `IN FILE`, DURATION, OFFSET, Dur, DATE, TIME, `AUTO ID*`, Fmin, Fmax) %>%
-    tidyr::separate(`IN FILE`, into = c("location", "recording_date_time"), sep = "(?:_0\\+1_|_|__0__|__1__)", extra = "merge", remove = F) %>%
+    tidyr::separate(`IN FILE`, into = c("location", "recordingDate"), sep = "(?:_0\\+1_|_|__0__|__1__)", extra = "merge", remove = F) %>%
     dplyr::select(-(DATE:TIME)) %>%
     dplyr::relocate(location) %>%
-    dplyr::relocate(recording_date_time, .after = location) %>%
-    dplyr::mutate(recording_date_time = stringr::str_remove(recording_date_time,'.+?(?:__)')) %>%
+    dplyr::relocate(recordingDate, .after = location) %>%
+    dplyr::mutate(recordingDate = stringr::str_remove(recordingDate,'.+?(?:__)')) %>%
     # Create date/time fields
-    dplyr::mutate(recording_date_time = lubridate::with_tz(lubridate::ymd_hms(recording_date_time), tzone = tz)) %>% #Apply a time zone if necessary
+    dplyr::mutate(recordingDate = lubridate::ymd_hms(recordingDate)) %>% #Apply a time zone if necessary
     dplyr::rename("taskLength" = 5,
                   "startTime" = 6,
                   "tagLength" = 7,
@@ -805,16 +804,16 @@ wt_kaleidoscope_tags <- function (input, output, tz, freq_bump = T) {
                                       species == "L_freq_Bat" ~ "LowF",
                                       TRUE ~ species),
                   startTime = dplyr::case_when(startTime == 0 ~ 0.1, TRUE ~ startTime)) %>% #Adjusting startTime parameter
-    tibble::add_column(method = "1SPT", .after = "recording_date_time") %>%
+    tibble::add_column(method = "1SPT", .after = "recordingDate") %>%
     tibble::add_column(transcriber = "Not Assigned", .after = "taskLength") %>%
-    dplyr::group_by(location, recording_date_time, taskLength, species) %>%
+    dplyr::group_by(location, recordingDate, taskLength, species) %>%
     dplyr::mutate(speciesIndividualNumber = row_number()) %>%
     dplyr::ungroup() %>%
     tibble::add_column(vocalization = "", .after = "speciesIndividualNumber") %>%
     tibble::add_column(abundance = 1, .after= "vocalization") %>%
     dplyr::mutate(vocalization = case_when(species == "Noise" ~ "Non-vocal", TRUE ~ "Call")) %>%
     tibble::add_column(internal_tag_id = "", .after = "maxFreq") %>%
-    dplyr::mutate(recording_date_time = as.character(recording_date_time)) %>%
+    dplyr::mutate(recordingDate = as.character(recordingDate)) %>%
     dplyr::rowwise() %>%
     dplyr::mutate(tagLength = dplyr::case_when(tagLength > taskLength ~ taskLength, TRUE ~ tagLength)) %>%
     dplyr::mutate(tagLength = dplyr::case_when(is.na(tagLength) ~ taskLength - startTime, TRUE ~ tagLength),
@@ -830,7 +829,8 @@ wt_kaleidoscope_tags <- function (input, output, tz, freq_bump = T) {
     dplyr::relocate(tagLength, .after = startTime) %>%
     dplyr::relocate(minFreq, .after = tagLength) %>%
     dplyr::relocate(maxFreq, .after = minFreq) %>%
-    dplyr::relocate(internal_tag_id, .after = maxFreq)
+    dplyr::relocate(internal_tag_id, .after = maxFreq) %>%
+    tidyr::drop_na()
 
   #Write the file
   return(write.csv(in_tbl_wtd, file = output, row.names = F))
