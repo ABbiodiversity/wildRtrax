603f9abd7c36d4c53a788c8749d0f951c3355ffa
Alexandre  MacPhail <alexandremacphail@Alexandres-MacBook-Pro.local>
2024-07-09 16:12:12 -0600

    coverage tests

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -16,47 +16,45 @@ bad_aoi <- list(
   c(-112.85438, 57.13472)
 )
 
-test_that("Download without authentication or boundary; single-species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = NULL)))
-})
-
-test_that("Download without authentication with boundary; single-species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = aoi)))
-})
-
-test_that("Download without authentication, bad boundary; single-species", {
-  expect_error(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = bad_aoi))
-})
-
-test_that("Download without authentication with boundary; multiple species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = aoi)))
-})
-
-test_that("Download without authentication, bad boundary; multiple species", {
-  expect_error(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = bad_aoi))
-})
-
-######
-
 Sys.setenv(WT_USERNAME = "guest", WT_PASSWORD = "Apple123")
-wt_auth(force = T)
+wt_auth(force = TRUE)
 
-test_that("Download with authentication, no boundary; single-species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = NULL)))
-})
-
-test_that("Download with authentication, boundary; single-species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = aoi)))
-})
-
-test_that("Download with authentication, boundary; multiple species", {
-  expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = aoi)))
-})
-
-test_that("Download with authentication, bad boundary; single-species", {
-  expect_error(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = bad_aoi))
-})
-
-test_that("Download with authentication, bad boundary; multiple species", {
-  expect_error(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = bad_aoi))
-})
+# test_that("Download without authentication or boundary; single-species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = NULL)))
+# })
+#
+# test_that("Download without authentication with boundary; single-species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = aoi)))
+# })
+#
+# test_that("Download without authentication, bad boundary; single-species", {
+#   expect_error(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = bad_aoi))
+# })
+#
+# test_that("Download without authentication with boundary; multiple species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = aoi)))
+# })
+#
+# test_that("Download without authentication, bad boundary; multiple species", {
+#   expect_error(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = bad_aoi))
+# })
+#
+# test_that("Download with authentication, no boundary; single-species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = NULL)))
+# })
+#
+# test_that("Download with authentication, boundary; single-species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = aoi)))
+# })
+#
+# test_that("Download with authentication, boundary; multiple species", {
+#   expect_true(!is.null(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = aoi)))
+# })
+#
+# test_that("Download with authentication, bad boundary; single-species", {
+#   expect_error(wt_dd_summary(sensor = 'ARU', species = 'White-throated Sparrow', boundary = bad_aoi))
+# })
+#
+# test_that("Download with authentication, bad boundary; multiple species", {
+#   expect_error(wt_dd_summary(sensor = 'ARU', species = c('White-throated Sparrow','Hermit Thrush'), boundary = bad_aoi))
+# })
